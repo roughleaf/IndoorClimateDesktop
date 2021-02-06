9abba1318713d55c6a7d63569f6da278d22ed008
HS Grobbelaar <roughleaf.development@gmail.com>
2021-02-06 09:45:49 +0200

    Added Services project and started bulding the models

---
+++ IndoorClimateDesktop.Services/ApiIndoorClimateLocalData/Models/ApiClimateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndoorClimateDesktop.Services.ApiIndoorClimateLocalData.Models
+{
+    class ApiClimateData
+    {
+        public string macAddress { get; set; }
+        public IEnumerable<ApiNodeData> NodeData { get; set; }
+        public IEnumerable<ApiLightningData> LightningData { get; set; }
+    }
+}
---
+++ IndoorClimateDesktop.Services/ApiIndoorClimateLocalData/Models/ApiLightningData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndoorClimateDesktop.Services.ApiIndoorClimateLocalData.Models
+{
+    class ApiLightningData
+    {
+        public int LightningDistance { get; set; }
+        public string DateStamp { get; set; }
+        public string TimeStamp { get; set; }
+    }
+}
